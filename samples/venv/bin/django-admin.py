323b399dc0787b2db35845bbfa8645c74f34f53d
sedhossein <seyedhosein77@gmail.com>
2020-08-11 02:07:46 +0430

    refactoring repo structure

--- samples/venv/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/home/sedhossein/Projects/github/ansible-learning/excersices/venv/bin/python
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
